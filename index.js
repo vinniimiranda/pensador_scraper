7463b054e8575adf15c718efa7e60648391cf0e8
Vinicius <vinniimiranda@gmail.com>
2020-06-15 17:42:30 -0300

    forEach in authors

--- index.js
+++ index.js
@@ -3,30 +3,32 @@ const cheerio = require('cheerio');
 const authors = require('./authors');
 const fs = require('fs');
 
+const results = [];
+
 authors
-  .filter((a, i) => i === 0)
+  .filter((a, i) => i <= 1)
   .forEach((author) => {
-    request(`https://www.pensador.com/autor/${author}`, (err, response, html) => {
+    const author_id = author.replace(' ', '_').toLocaleLowerCase();
+    request(`https://www.pensador.com/autor/${author_id}`, (err, response, html) => {
       if (err) {
         console.log('Erro');
       }
 
       if (!err && response.statusCode === 200) {
         const $ = cheerio.load(html);
-        const results = [];
 
         const authorTotal = $('.autorTotal');
         const totalThoughts = authorTotal.children().last().text();
 
         const pagination = parseInt(totalThoughts / 25 + 1);
 
-        for (let i = 1; i < pagination + 1; i++) {
+        for (let i = 1; i < 2; i++) {
           request(`https://www.pensador.com/autor/${authorTotal[0]}/${i}`, (error, res, html2) => {
             const $ = cheerio.load(html);
             const phrase = $('.frase');
 
             phrase.each((i, p) => {
-              results.push({ author: author, phrase: p.childNodes[0].data });
+              results.push({ author, author_id, phrase: p.childNodes[0].data });
             });
 
             fs.writeFileSync('./results.json', JSON.stringify(results));
