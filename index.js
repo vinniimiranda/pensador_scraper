a905856e12e6a1433d12ccf8dc0f4e00acb6131b
Vinicius <vinniimiranda@gmail.com>
2020-06-15 19:42:38 -0300

    Remove pagination

--- index.js
+++ index.js
@@ -7,24 +7,15 @@ const results = [];
 
 (async function () {
   for await (const author of authors) {
-    const author_id = author.replace(' ', '_').toLocaleLowerCase();
-    const { data } = await axios.get(`https://www.pensador.com/autor/${author_id}`);
+    const author_id = author.replace(/\s/g, '_').toLocaleLowerCase();
 
+    const { data } = await axios.get(`https://www.pensador.com/autor/${author_id}/1`);
     const $ = cheerio.load(data);
+    const phrase = $('.frase');
 
-    const authorTotal = $('.autorTotal');
-    const totalThoughts = authorTotal.children().last().text();
-    const pagination = parseInt(totalThoughts / 25 + 1);
-
-    for (let i = 1; i < pagination + 1; i++) {
-      const { data: html } = await axios.get(`https://www.pensador.com/autor/${author}/${i}`);
-      const $ = cheerio.load(html);
-      const phrase = $('.frase');
-
-      phrase.each((i, p) => {
-        results.push({ author, author_id, phrase: p.childNodes[0].data });
-      });
-    }
+    phrase.each((i, p) => {
+      results.push({ author, author_id, phrase: p.childNodes[0].data });
+    });
 
     fs.writeFileSync('./results.json', JSON.stringify(results));
   }
